ebe1c660c36b69262861375eedeffdf6717512e3
Mark LeMoine <thatsgobbles@gmail.com>
2019-11-20 17:50:50 -0800

    Bringing over structs and concepts from example

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,77 @@
 #![recursion_limit = "512"]
 
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
 use yew::Component;
 use yew::ComponentLink;
 use yew::ShouldRender;
 use yew::Html;
 use yew::html;
+use yew::format::Json;
+use yew::services::storage::Area;
+use yew::services::storage::StorageService;
 
-pub enum Model {
-    Sample,
+const KEY: &'static str = "yew-sample-todo.self";
+
+pub struct Model {
+    storage: StorageService,
+    state: State,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct State {
+    entries: Vec<Entry>,
+    filter: Filter,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Entry {
+    text: String,
+    completed: bool,
+}
+
+pub enum Message {
+    Create,
+    Delete(usize),
+    Update(usize, String),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Filter {
+    All,
+    Active,
+    Done,
+}
+
+impl Default for Filter {
+    fn default() -> Self {
+        Self::All
+    }
 }
 
 impl Component for Model {
-    type Message = ();
+    type Message = Message;
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model::Sample
+        let storage = StorageService::new(Area::Local);
+
+        let entries = {
+            if let Json(Ok(restored_entries)) = storage.restore(KEY) {
+                restored_entries
+            } else {
+                Vec::new()
+            }
+        };
+
+        let filter = Filter::default();
+
+        let state = State {
+            entries,
+            filter,
+        };
+
+        Model { storage, state, }
     }
 
     fn update(&mut self, _: Self::Message) -> ShouldRender {
